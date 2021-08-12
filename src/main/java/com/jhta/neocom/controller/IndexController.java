24e565a237a84f705508b2e49db700032c1ba5da
yong <dahlia7102@gmail.com>
2021-08-12 19:23:01 +0900

    .

--- src/main/java/com/jhta/neocom/controller/IndexController.java
+++ src/main/java/com/jhta/neocom/controller/IndexController.java
@@ -6,12 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/admin/")
-    public String adminIndex() {
-
-        return "admin/index";
-    }
-
+   
     
     @RequestMapping(value = "/")
     public String frontendIndex() {
