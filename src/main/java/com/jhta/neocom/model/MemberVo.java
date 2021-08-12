b384226da16777748ec9b33c9313728e9c3bfb30
yong <dahlia7102@gmail.com>
2021-08-12 19:30:50 +0900

    validation어노테이션으로 유효성검사, 메시지입력

--- src/main/java/com/jhta/neocom/model/MemberVo.java
+++ src/main/java/com/jhta/neocom/model/MemberVo.java
@@ -32,7 +32,7 @@ public class MemberVo {
 	String id;
 	@NotBlank
 	@Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-    message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    message = "영문 대,소문자와 숫자, 특수기호 1 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다")
 	String password;
 	
 	
